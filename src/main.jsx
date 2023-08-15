a6de48b3b0aad983dfaca5a46db4a57274bf32dc
jo-oo <johannakoreo@gmail.com>
2023-08-15 16:45:28 +0200

    set up and configure query client

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 2, //2 minutes
+			cacheTime: 1000 * 60 * 60 * 3, //3 hours
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>
 )
