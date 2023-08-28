a8b8002a0d4d5e73e9ab7184efadddf039c2c83c
jo-oo <johannakoreo@gmail.com>
2023-08-28 20:20:25 +0200

    toggeling dropdown when clicking on icon in small screens

--- src/Layout/Navigation/NavBar.jsx
+++ src/Layout/Navigation/NavBar.jsx
@@ -10,42 +10,48 @@ const NavBar = () => {
 
     return (
         <div className="sticky top-0 left-0 right-0">
-        <nav className="w-full bg-slate-200">
-            <div className="justify-between px-4 mx-auto xl:max-w-screen-xl md:items-center md:flex md:px-8">
-                <div>
-                    <div className="flex items-center justify-between py-5 md:block">
-                        <a href="">
-                            <h2 className="text-2xl font-bold">LOGO</h2>
-                        </a>
-                        <div className="md:hidden">
-                        <button
-                                className="text-gray-600"
-                                onClick={() => setDropDown(!dropDown)}
-                            >
-                                {!dropDown ? (
-                                     <FontAwesomeIcon icon={faBars} className="text-[1.4rem]"/>
-                                ) : (
-                                    <FontAwesomeIcon icon={faX} className="text-[1.35rem]"  />
-                                )}
-                            </button>
-                        </div>
-                    </div>  
-                </div>
-                <div>
+            <nav className="w-full bg-slate-200">
+                <div className="justify-between px-4 mx-auto xl:max-w-screen-xl md:items-center md:flex md:px-8">
                     <div>
-                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                            <li className="text-gray-600 hover:text-green-400">
-                                <a href="">Home</a>
-                            </li>
-                            <li className="text-gray-600 hover:text-green-400">
-                                <a href="">Products</a>
-                            </li>
-                        </ul>
+                        <div className="flex items-center justify-between py-5 md:block">
+                            <a href="">
+                                <h2 className="text-2xl font-bold">LOGO</h2>
+                            </a>
+                            {/* shows dropdown menu in screens smaller than medium*/}
+                            <div className="md:hidden">
+                                {/* toggeling dropdown icon and close icon */}
+                                <button
+                                    className="text-gray-600"
+                                    onClick={() => setDropDown(!dropDown)}
+                                >
+                                    {!dropDown ? (
+                                        <FontAwesomeIcon icon={faBars} className="text-[1.4rem]"/>
+                                    ) : (
+                                        <FontAwesomeIcon icon={faX} className="text-[1.35rem]"  />
+                                    )}
+                                </button>
+                            </div>
+                        </div>  
+                    </div>
+                    <div>
+                        {/* toggeling dropdown when clicking on icon in small screens */}
+                        <div className={`flex-1 justify-self-center pb-3 mt-5 pb-5 md:block md:pb-0 md:mt-0 ${
+                                dropDown ? "block" : "hidden"
+                            }`}
+                        >
+                            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                                <li className="text-gray-600 hover:text-green-400">
+                                    <a href="">Home</a>
+                                </li>
+                                <li className="text-gray-600 hover:text-green-400">
+                                    <a href="">Products</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    </div>
+            </nav>
+        </div>
     )
 }
 
