bbb3c11ff3dd24acd6a93c9afca52651baec3348
jo-oo <johannakoreo@gmail.com>
2023-08-29 22:19:02 +0200

    Home Page basic layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,25 @@
+import { Link } from 'react-router-dom'
+
+/** Icons **/
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
+
 const HomePage = () => {
     return (
-        <div>Home Page</div>
+        <div>
+            <div className="w-full place-items-center overflow-hidden ">
+                <Link to={`/products/`}>
+                    <div className="bg-slate-50 p-40 mt-[110px] xs:p-10 lg:p-30 md:p-20 place-items-center text-5xl">
+                        <div className="">
+                            GET READY FOR OUR NEW AUTUMN ARRIVALS. JUST A STEP AWAY. --
+                            <FontAwesomeIcon icon={faChevronRight}
+                            className="text-4xl"
+                            />
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </div>
     )
 }
 
