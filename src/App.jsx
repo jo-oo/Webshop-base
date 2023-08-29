48864758dba4ec2e309f046f691ca3b2cb1ede7d
jo-oo <johannakoreo@gmail.com>
2023-08-29 14:06:16 +0200

    add PageContentWrapper set with margin to navbar (page top)

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import ProductsPage from './pages/ProductsPage'
 import ProductPage from './pages/ProductPage'
 import NotFound from './pages/NotFound'
 import NavBar from './Layout/Navigation/NavBar'
+import PageContentWrapper from './Layout/Content/PageContentWrapper'
 
 function App() {
-
     return (
         <div>
-            <div className="">
-				<NavBar />  
-			</div>
-            <Routes>
-				<Route path="/" element={<HomePage />} />
-                <Route path="/products" element={<ProductsPage />} />
-				<Route path="/products/:id" element={<ProductPage />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <NavBar />  
+            <PageContentWrapper>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/products" element={<ProductsPage />} />
+                    <Route path="/products/:id" element={<ProductPage />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </PageContentWrapper>
         </div>
     )
 }
---
+++ src/Layout/Content/PageContentWrapper.jsx
@@ -0,0 +1,9 @@
+const PageContentWrapper = ({children}) => {
+    return (
+        <div className={`mt-20`}>
+            {children}
+        </div>
+    )
+}
+
+export default PageContentWrapper
